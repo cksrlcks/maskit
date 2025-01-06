6cb0a4fa16fee77174ea46fa8743958f520e5e67
chanki.kim <chanki.kim89@gmail.com>
2025-01-06 20:29:39 +0900

    Design: 요술봉 효과 넣기

--- src/components/layer/CanvasMenu.tsx
+++ src/components/layer/CanvasMenu.tsx
@@ -199,10 +199,16 @@ export function CanvasMenu() {
             {isOCRLoading ? (
               <Loader2 className="animate-spin" />
             ) : (
-              <WandSparkles className="h-[1.2rem] w-[1.2rem]" />
-            )}
+              <>
+                <WandSparkles className="h-[1.2rem] w-[1.2rem]" />
+                <span className="sr-only">자동 그리기</span>
 
-            <span className="sr-only">자동 그리기</span>
+                <span className="absolute right-1.5 top-1.5 flex h-2 w-2">
+                  <span className="absolute inline-flex h-full w-full animate-ping rounded-full bg-sky-400 opacity-75"></span>
+                  <span className="relative inline-flex h-2 w-2 rounded-full bg-sky-500"></span>
+                </span>
+              </>
+            )}
           </Button>
         </div>
       </div>
