8066e9419973e97977d26d0387b28d5699b8cd5c
chanki.kim <chanki.kim89@gmail.com>
2025-01-14 16:35:02 +0900

    fix: 드롭다운, 구글드라이브 아이콘 테마에 반응하도록 수정

--- src/components/Canvas/Upload.tsx
+++ src/components/Canvas/Upload.tsx
@@ -8,8 +8,6 @@ import {
   DropdownMenuItem,
   DropdownMenuTrigger,
 } from "@/components/ui";
-import dropboxIcon from "@/assets/img/icon-dropbox.svg";
-import googleIcon from "@/assets/img/icon-drive.svg";
 import { useDropzone } from "react-dropzone";
 import clsx from "clsx";
 import useGoogleDrive from "@/hooks/useGoogleDrive";
@@ -132,11 +130,23 @@ export function Upload() {
                       PC에서 가져오기
                     </DropdownMenuItem>
                     <DropdownMenuItem onClick={handleDropboxPicker}>
-                      <img src={dropboxIcon} alt="dropdown" className="h-4 w-4" />
+                      <svg
+                        xmlns="http://www.w3.org/2000/svg"
+                        viewBox="0 0 24 24"
+                        fill="currentColor"
+                      >
+                        <path d="M17.2847 10.6683L22.5 13.9909L17.248 17.3368L12 13.9934L6.75198 17.3368L1.5 13.9909L6.7152 10.6684L1.5 7.34587L6.75206 4L11.9999 7.34335L17.2481 4L22.5 7.34587L17.2847 10.6683ZM17.2112 10.6684L11.9999 7.3484L6.78869 10.6683L12 13.9883L17.2112 10.6684ZM6.78574 18.4456L12.0377 15.1L17.2898 18.4456L12.0377 21.7916L6.78574 18.4456Z"></path>
+                      </svg>
                       Dropbox
                     </DropdownMenuItem>
                     <DropdownMenuItem onClick={handleGooglePicker}>
-                      <img src={googleIcon} alt="Google drive" className="h-4 w-4" />
+                      <svg
+                        xmlns="http://www.w3.org/2000/svg"
+                        viewBox="0 0 24 24"
+                        fill="currentColor"
+                      >
+                        <path d="M7.94037 4.14596L11.4217 10.1759L5.48201 20.4688L2 14.44L7.94037 4.14596ZM10.1161 14.44H22L18.518 20.4688H6.63537L10.1161 14.44ZM14.4589 13.4399L8.51799 3.14502H15.482L21.4229 13.4399H14.4589Z"></path>
+                      </svg>
                       Google drive
                     </DropdownMenuItem>
                   </DropdownMenuContent>
