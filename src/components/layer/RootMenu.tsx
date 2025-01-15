3b4b75f944791eba89d4ebc88994b674f1d5c35f
chanki.kim <chanki.kim89@gmail.com>
2025-01-15 22:14:14 +0900

    refactor: hidden을 sronly로 교체

--- src/components/layer/RootMenu.tsx
+++ src/components/layer/RootMenu.tsx
@@ -132,7 +132,7 @@ export function RootMenu() {
           className="max-w-[400px] overflow-hidden rounded-md border-none bg-transparent p-0 shadow-none"
           showCloseButton={false}
         >
-          <DialogHeader className="hidden">
+          <DialogHeader className="sr-only">
             <DialogTitle>MAKIT 소개</DialogTitle>
             <DialogDescription>MAKIT 소개</DialogDescription>
           </DialogHeader>
